621af8ed9cdb96419c47ff6dbdfc2b19382cf752
Miłosz Krawczyk <mixol456@gmail.com>
2022-11-25 12:37:25 +0100

    added intersection observer scrollspy

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,59 @@
 const burgerBtn = document.querySelector('.burger-btn')
 const navActive = document.querySelector('.nav__mobile')
 const mobileItem = document.querySelectorAll('.nav__item')
+const nav = document.getElementById('spy-nav')
+const sections = [...document.querySelectorAll('.section')]
 
 function mobileNav() {
 	navActive.classList.toggle('nav__mobile--active')
-    
-const navAnimation = () => {
+
+	const navAnimation = () => {
 		let delayTime = 0
 
 		mobileItem.forEach(item => {
-            item.classList.toggle('mobile__animation')
+			item.classList.toggle('mobile__animation')
 			item.style.animationDelay = '.' + delayTime + 's'
 			delayTime++
 		})
-        
 	}
-	navAnimation ()
+	navAnimation()
 }
 
-
 burgerBtn.addEventListener('click', mobileNav)
+
+// SCROLLSPY
+
+
+    
+	let options = {
+	
+	threshold: .6,
+  }
+  
+  const spyNav = (entries, observer) => {
+	
+	  entries.forEach((entry) => {
+
+		  console.log(entry);
+
+		  const { id } = entry.target;
+		  const spyNav = nav.querySelector(`[href="#${id}"`);
+	  
+		  spyNav.classList.remove("nav__items-item--active");
+
+		  if (!entry.isIntersecting) return;
+		  
+		  spyNav.classList.add("nav__items-item--active");
+		})
+	}
+	
+	const observer = new IntersectionObserver(spyNav, options);
+	
+	
+	sections.forEach( section => {
+		observer.observe(section);
+		})
+  
+  
+
+
---
+++ src/js/mobileNav.js
@@ -0,0 +1,25 @@
+export const mobileNav = function nav (){
+
+const burgerBtn = document.querySelector('.burger-btn')
+const navActive = document.querySelector('.nav__mobile')
+const mobileItem = document.querySelectorAll('.nav__item')
+
+ function mobileNav() {
+	navActive.classList.toggle('nav__mobile--active')
+    
+const navAnimation = () => {
+		let delayTime = 0
+
+		mobileItem.forEach(item => {
+            item.classList.toggle('mobile__animation')
+			item.style.animationDelay = '.' + delayTime + 's'
+			delayTime++
+		})
+        
+	}
+	navAnimation ()
+}
+
+
+burgerBtn.addEventListener('click', mobileNav)
+}
